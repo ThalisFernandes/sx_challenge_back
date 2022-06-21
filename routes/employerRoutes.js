b2823e2673e08ce09dd52dd108656408c4040ddf
Thalis Fernandes <106416519+ThalisFernandes@users.noreply.github.com>
2022-06-21 05:45:06 -0300

    Update employerRoutes.js

--- routes/employerRoutes.js
+++ routes/employerRoutes.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection(
     {
         host:"localhost",
-        user:"root",
-        password:"30912650",
+        user:"YourUserName",
+        password:"YourPassWord",
         database: 'sx_challenge'
     }
 );
